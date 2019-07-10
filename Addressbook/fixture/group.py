5b2dc42c5c448375a01940bc46a5a3f17bb5c7fb
smagdenko <magdenko_ne@ukr.net>
2019-07-10 18:02:52 +0300

    change xpath for modify_group

--- Addressbook/fixture/group.py
+++ Addressbook/fixture/group.py
@@ -1,24 +1,20 @@
-from selenium import webdriver
-
 class GroupSession:
 
     def __init__(self, app):
         self.app=app
 
     def create(self, group):
-
         driver = self.app.driver
         driver.find_element_by_xpath(".//*[@id='nav']/ul/li[3]/a").click()
         driver.find_element_by_xpath(".//*[@id='content']/form/input[1]").click()
-        # New group
-        self.fill_group_name(group)
+        self.fill_group_form(group)
 
-    def fill_group_name(self, group):
+    def fill_group_form(self, group):
         driver = self.app.driver
-        self.change_field_text("field_name", group.name)
-        self.change_field_text("field_header", group.header)
-        self.change_field_text("field_footer", group.footer)
-        driver.find_element_by_name("submit").click()
+        self.change_field_text("group_name", group.name)
+        self.change_field_text("group_header", group.header)
+        self.change_field_text("group_footer", group.footer)
+        driver.find_element_by_name("update").click()
 
     def change_field_text(self, field_name, text):
         driver = self.app.driver
@@ -27,25 +23,28 @@ class GroupSession:
             driver.find_element_by_name(field_name).clear()
             driver.find_element_by_name(field_name).send_keys(text)
 
+    def select_first_group(self):
+        driver = self.app.driver
+        driver.find_element_by_xpath("//*[@id='content']//span[1]").click()
 
     def delete(self):
         driver = self.app.driver
         driver.find_element_by_xpath(".//*[@id='nav']/ul/li[3]/a").click()
         groups=driver.find_elements_by_xpath("//*[@id='content']//span")
         for _ in range(len(groups)):
-            driver.find_element_by_xpath("//*[@id='content']//span[1]").click()
+            self.select_first_group()
             driver.find_element_by_xpath("//input[2][@name='delete']").click()
             driver.find_element_by_xpath(".//*[@id='content']//a[contains(.,'group page')]").click()
 
     def modify_first_group(self, new_group_data):
         driver = self.app.driver
-
         # driver.find_element_by_xpath(".//*[@id='maintable']//tr[2]//input").click()
         driver.find_element_by_xpath("//*[@id='nav']//a[contains(.,'groups')]").click()
+        self.select_first_group()
         # open modification form
         driver.find_element_by_xpath("//input[3][@name='edit']").click()
         # fill group form
-        self.fill_group_name(new_group_data)
+        self.fill_group_form(new_group_data)
         # submit modification
         driver.find_element_by_xpath(".//*[@id='content']/form/input[3]").click()
         driver.find_element_by_xpath(".//*[@id='content']//a[contains(.,'group page')]").click()
--- Addressbook/test/test_modify_group.py
+++ Addressbook/test/test_modify_group.py
@@ -3,5 +3,5 @@ from model.group import Group
 
 def test_modify_group(app):
     app.login(user="admin", password="secret")
-    app.group.modify_first_group(Group(name="qqqqqqqqqqqqqq"))
+    app.group.modify_first_group(Group(name="group"))
     app.logout()
